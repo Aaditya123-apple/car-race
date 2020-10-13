06a72ca9f111688e43f5727bfcf1e558745dc8c7
Aaditya123-apple <68804634+Aaditya123-apple@users.noreply.github.com>
2020-10-13 11:02:18 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,52 @@
+class Player{
+  
+    constructor(){
+        this.name = null;
+        this.distance = 0;
+        this.index = null;
+
+    }
+
+getCount(){
+
+    var playerCountref = database.ref('playerCount')
+playerCountref.on("value", function(data){
+
+playerCount = data.val()
+
+})
+}
+
+updateCount(count){
+
+database.ref('/').update({
+
+playerCount: count
+
+})
+}
+
+update(){
+
+var playerIndex = "players/player" + this.index;
+
+database.ref(playerIndex).set({
+
+name: this.name,
+distance: this.distance
+
+})
+}
+
+static getPlayerinfo(){
+   
+    var Playerinforef = database.ref('players');
+    Playerinforef.on("value", (data)=>{
+        allPlayers = data.val();
+    })
+
+    
+
+}
+
+}
